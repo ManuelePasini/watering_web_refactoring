634178a8867cecca46e46fa40f3ad2f6b771c82d
manuelepasini <manuele.pasini@unibo.it>
2024-06-13 15:55:09 +0000

    add build image

---
+++ frontend/build_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VERSION="${2:-v0.0.1}"
+IMAGE_NAME=127.0.0.0:5000/$1:$VERSION
+
+#Second parameter is optional and it's version, by default its v0.0.1
+
+
+docker build --tag $IMAGE_NAME -f ./Dockerfile .
+docker push $IMAGE_NAME
