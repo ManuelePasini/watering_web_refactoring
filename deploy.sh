58fec3539e25608781b033fe49172d547cca9527
alexbaiardi <alex.baiardi99@gmail.com>
2024-07-04 12:54:14 +0200

    fix: path bug

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 cd backend
 ./build_image.sh watering-web-backend
 cd ../frontend
-./frontend/build_image.sh watering-web-frontend
+./build_image.sh watering-web-frontend
 cd ..
 docker stack deploy -c ./docker-compose.yaml watering-website
